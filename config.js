4157608e392973bbb595533f9757a7e6ece77afd
Sonrisa <sonrisa722611@gmail.com>
2017-08-04 15:40:53 -0700

    add simple local host server via express

--- config.js
+++ config.js
@@ -1,2 +1,4 @@
-console.log('config');
-
+export default {
+  port: 8080,
+  ip: '127.0.0.1',
+};
--- server.js
+++ server.js
@@ -1,2 +1,16 @@
-import './config';
-console.log('a');
+import config from './config';
+import express from 'express';
+
+console.log(config)
+const server = express();
+
+server.get('/', (req, res) => {
+  res.send('Hello Express');
+});
+
+server.listen(config.port, () => {
+  console.log('Listening on http://' + config.ip + ':' + config.port);
+});
+
+
+
