0f7206a775016321ceb78c4207d2f6a90691a263
Sonrisa <sonrisa722611@gmail.com>
2017-08-07 02:23:33 -0700

    update data base

--- loadBeanData.js
+++ loadBeanData.js
@@ -1,72 +1,55 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/beandb');
+import { MongoClient } from 'mongodb';
+import assert from 'assert';
+import config from './config';
 
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log( "we're connected!");
+MongoClient.connect(config.mongodbUri, (err, db) => {
+  assert.equal(null, err);
 
+  db.collection('Beans').insertMany([
+    { id: 1,
+      categoryName: 'darkRoast',
+      beanName: 'Sumatran Mandeling',
+      waterTemp: '203F',
+      weight:'28g',
+      grindFineness:'medium coarse',
+      ,
+      regionIds: [101] },
+    { id: 2,
+      categoryName: 'medRoast',
+      beanName: 'Kenya AA',
+      waterTemp: '205F',
+      weight:'25g',
+      grindFineness:'medium coarse',
+      ,
+      regionIds: [102] },
+    { id: 2,
+      categoryName: 'lightRoast',
+      beanName: 'Costa Rica',
+      waterTemp: '208F',
+      weight:'23g',
+      grindFineness:'medium coarse',
+      ,
+      regionIds: [103] },
+    { id: 2,
+      categoryName: 'lightRoast',
+      beanName: 'Ethiopia',
+      waterTemp: '200F',
+      weight:'25g',
+      grindFineness:'medium coarse',
+      ,
+      regionIds: [104] },
+
+
+  ]).then(response => {
+    console.info('Beans', response.insertedCount);
+    db.collection('Region').insertMany([
+      { id: 101, regionName: 'Indonesia', timestamp: new Date() },
+      { id: 102, regionName: 'Kenya', timestamp: new Date() },
+      { id: 103, regionName: 'Tarrazu Costa Risa', timestamp: new Date() },
+      { id: 104, regionName: 'Nembo, Sidamo', timestamp: new Date() },
+    ]).then(response => {
+      console.info('Region', response.insertedCount);
+      db.close();
+    });
+  });
 });
-
-
-var BeanSchema = mongoose.Schema({
-    name: String,
-    region: String,
-    description: String
-});
-
-
-
-
-var Bean = mongoose.model('Bean', BeanSchema);
-
-
-var Kenya = new Bean({
-  name: 'Kenya',
-    region: 'Indonesia',
-    description: 'Dark roast'
-
-});
-
-
-Kenya.save(function(err, result) {
-  if(err){console.log(err)};
-  console.log("yeah", result);
-});
-
-
-var sumatra = new Bean({
-  name: 'Sumatra',
-    region: 'Indonesia',
-    description: 'Dark roast'
-
-});
-
-
-sumatra.save(function(err, result) {
-  if(err){console.log(err)};
-  console.log("yeah", result);
-});
-
-var costaRica= new Bean({
- name: 'Costa Rica',
-    region: 'Costa Rica',
-    description: 'Dark roast'
-
-});
-
-
-costaRica.save(function(err, result) {
-  if(err){console.log(err)};
-  console.log("yeah", result);
-});
-
-// var MongoClient = require('mongodb').MongoClient;
-// var url = "mongodb://localhost:27017/beandb";
-
-// MongoClient.connect(url, function(err,db) {
-//   if(err) throw err;
-//   console.log("Cofforest created!!");
-//   db.close();
-
-// });
